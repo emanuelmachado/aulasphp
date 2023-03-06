559e3f3ccc81d9787e164034ad19b70627929a42
Emanuel Machado <egodoymachado@hotmail.com>
2023-03-06 18:15:48 -0300

    alteração de textos
    remoção de texto
    
    adição de textos
    
    reção de linha vazia ou console.log

--- squash.php
+++ squash.php
@@ -6,5 +6,15 @@ echo "teste 3";
 echo "teste 3";
 echo "teste 2";
 echo "teste 3";
+
+
+echo "teste 3";
+echo "teste 3";
+echo "teste 2";
 echo "teste 3";
 
+echo "teste 3";
+echo "teste 3";
+echo "teste 2";
+echo "teste 3";
+echo "teste 3";
